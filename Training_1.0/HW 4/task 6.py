af6c29fd5ac001c5e8d22fe1d1d5d1794f00d062
iktkhor <txor2009@mail.ru>
2023-02-21 12:02:04 +0300

    tr 1 hw 4 task 6

---
+++ Training_1.0/HW 4/task 6.py
@@ -0,0 +1,21 @@
+buyers = {}
+with open('input.txt') as text:
+    for line in text:
+        data = line.split()
+        name = data[0]
+        product = data[1]
+        amount = data[2]
+        if name not in buyers:
+            buyers[name] = {product: 0}
+        elif product not in buyers[name]:
+            buyers[name][product] = 0
+        buyers[name][product] += int(amount)
+
+        #print(buyers[name][product])
+for buyer in sorted(buyers.keys()):
+    print(buyer + ':')
+    for item in sorted(buyers[buyer].keys()):
+        print(item, buyers[buyer][item])
+
+
+
